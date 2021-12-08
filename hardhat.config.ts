c46ffc2f884797b32d11f5eb9808d048a4cc280f
Thang Le <jakethecat21@gmail.com>
2021-12-08 17:11:59 +0700

    contract publish and verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -58,7 +58,7 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.BSC_SCAN_KEY,
   },
   namedAccounts: {
     deployer: 0,

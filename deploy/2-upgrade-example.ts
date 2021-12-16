dbd69c2fd50362525cf883649604c2571ac5792e
Thang Le <jakethecat21@gmail.com>
2021-12-16 11:01:09 +0700

    new deployment scripts and rinkeby deployment results

--- deploy/2-upgrade-example.ts
+++ deploy/2-upgrade-example.ts
@@ -2,15 +2,28 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
-  const { deployer, stakingAddress } = await getNamedAccounts();
+  const { deployments, getNamedAccounts, ethers } = hre;
+  const { deployer } = await getNamedAccounts();
 
-  await deploy('TestUpgrade', {
-    from: deployer,
-    args: [],
-    log: true,
-  });
+  const implDeployResult = await deployments.deploy(
+    'TestUpgrade_Implementation',
+    {
+      from: deployer,
+      args: [],
+      contract: 'TestUpgrade',
+    }
+  );
+
+
+  const upgradeTx = await deployments.execute(
+    'Token',
+    { from: deployer },
+    'upgradeTo',
+    implDeployResult.address,
+  );
+
+
+  console.log('upgradeTx', upgradeTx)
 };
 
 func.tags = ['upgrade-example'];
